e1b07ba95fb1a5c88b8d73bff0ba3277ea0aec52
DJ Mitchell <dj_trumpet@hotmail.com>
2016-08-17 14:48:19 -0400

    Add API key to network requests

--- GiphySearch/GiphySearch/Networking/GiphyAPI.swift
+++ GiphySearch/GiphySearch/Networking/GiphyAPI.swift
@@ -25,34 +25,40 @@ extension GiphyAPI: TargetType {
             return "trending"
         }
     }
-    
+
     var method: Moya.Method {
         return .GET
     }
-    
+
     var parameters: [String : AnyObject]? {
         // Hard code the number of items per page.
         // This is the same as Giphy's default but we need it to calculate our offset,
         // so we will just pass it regardless
         let itemsPerPage = 25
-        
+
+        var params = [String:AnyObject]()
         switch self {
         case .search(let searchString):
             // TODO: - Pagination
-            return [
+            params = [
                 "q" : searchString.lowercaseString.stringByReplacingOccurrencesOfString(" ", withString: "+"),
                 "rating" : "r", // Keep it dirty, default to R rated
             ]
         case .trending(let page):
             let offset = itemsPerPage * page
-            
-            return [
+
+            params = [
                 "limit": itemsPerPage,
                 "offset": offset
             ]
         }
+
+        // Always append API key
+        params["api_key"] = "dc6zaTOxFJmzC"
+
+        return params
     }
-    
+
     var sampleData: NSData {
         // TODO: - Stubbed responses for testing
         return NSData()
