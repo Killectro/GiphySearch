129cef61a817f563aa74320fc64daef91ac4ea0a
DJ Mitchell <dj_trumpet@hotmail.com>
2016-08-17 13:34:18 -0400

    Add view model and clean up view controller

---
+++ GiphySearch/GiphySearch/TrendingViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  TrendingViewModel.swift
+//  GiphySearch
+//
+//  Created by DJ Mitchell on 8/17/16.
+//  Copyright © 2016 Killectro. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import Moya
+
+final class TrendingViewModel: NSObject {
+    
+}
--- GiphySearch/GiphySearch/View Controllers/Trending/TrendingViewController.swift
+++ GiphySearch/GiphySearch/View Controllers/Trending/TrendingViewController.swift
@@ -8,28 +8,21 @@
 
 import UIKit
 
-class TrendingViewController: UIViewController {
+final class TrendingViewController: UIViewController {
 
+    // MARK: - Public Properties
+    var viewModel: TrendingViewModel!
+    
+    // MARK: - Private Properties
+    @IBOutlet private var tableView: UITableView!
+    @IBOutlet private var searchBar: UISearchBar!
+    
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
